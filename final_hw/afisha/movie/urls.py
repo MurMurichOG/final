9d812a7dc72ead8634883c031c368cc06fd4e037
MurMurichOG <muratmuhutdinov1122@gmail.com>
2024-02-27 20:52:24 +0600

    hws

---
+++ final_hw/afisha/movie/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('api/v1/directors/', views.directors),
+    path('api/v1/directors/<int:id>/', views.directors_detail),
+    path('api/v1/movies/', views.movies),
+    path('api/v1/movies/<int:id>/', views.movies_detail),
+    path('api/v1/reviews/', views.reviews),
+    path('api/v1/reviews/<int:id>/', views.reviews_detail)
+]
